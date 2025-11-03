220b8d9dfb80b1d1e6acc255192688d18b02a1b9
Shanush <skmshanush07@gmail.com>
2025-11-03 22:50:21 +0530

    api.js mistake
    I didnt update the backend url for api.js. SO NOW ITS FIXED :)

--- cycle-rental-app/frontend/src/api.js
+++ cycle-rental-app/frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { useAuth } from "@clerk/clerk-react";
 
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
 
 export function useApi() {
   const { getToken } = useAuth();
